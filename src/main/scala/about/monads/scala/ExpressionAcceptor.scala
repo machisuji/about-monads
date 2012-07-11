4d5171e76b1cbac29bc65e67532dcc5fb22e27c1
Markus Kahl <machisuji@googlemail.com>
2012-07-11 09:48:34 +0200

    scala acceptor now calculates too

--- src/main/scala/about/monads/scala/ExpressionAcceptor.scala
+++ src/main/scala/about/monads/scala/ExpressionAcceptor.scala
@@ -11,26 +11,53 @@ import java.util.Scanner
  */
 object ExpressionAcceptor {
 
-  def expr(tokens: List[String]): Step = (Accept(tokens) >>=
-    open >>= expr >>= operator >>= expr >>= close) orElse (Accept(tokens) >>= number)
-
-	def number(tokens: List[String]): Step = tokens match {
-    case token::rest if new Scanner(token).hasNextInt => Accept(rest)
-    case _ => Reject("Expected number, got: " + tokens.headOption.mkString, tokens)
+  def expr(value: Option[String], tokens: List[String]): Step = {
+    val input = Accept(value, tokens)
+    val step = input >>= open >>= expr >>= operator >>= expr >>= close
+    
+    (step >>= calculate) orElse (input >>= number)
   }
 
-  def operator(tokens: List[String]): Step = tokens match {
-    case token::rest if token.size == 1 && "+-*/".contains(token) => Accept(rest)
-    case _ => Reject("Expected operator, got: " + tokens.headOption.mkString, tokens)
+	def number(value: Option[String], tokens: List[String]): Step = tokens match {
+    case token::rest if new Scanner(token).hasNextInt =>
+      Accept(value.map(_ + token) orElse Some(token), rest)
+    case _ =>
+      Reject("Expected number, got: " + tokens.headOption.mkString, tokens)
   }
 
-  def open(tokens: List[String]): Step = tokens match {
-    case token::rest if token == "(" => Accept(rest)
+  def operator(value: Option[String], tokens: List[String]): Step = tokens match {
+    case token::rest if token.size == 1 && "+-*/".contains(token) =>
+      Accept(value.map(_ + " " + token + " "), rest)
+    case _ =>
+      Reject("Expected operator, got: " + tokens.headOption.mkString, tokens)
+  }
+
+  def open(value: Option[String], tokens: List[String]): Step = tokens match {
+    case token::rest if token == "(" => Accept(value, rest)
     case _ => Reject("Expected \"(\", got: " + tokens.headOption.mkString, tokens)
   }
 
-  def close(tokens: List[String]): Step = tokens match {
-    case token::rest if token == ")" => Accept(rest)
+  def close(value: Option[String], tokens: List[String]): Step = tokens match {
+    case token::rest if token == ")" => Accept(value, rest)
     case _ => Reject("Expected \")\", got: " + tokens.headOption.mkString, tokens)
+  }
+
+  def calculate(value: Option[String], tokens: List[String]): Step = {
+    val Op = """(\d+) ([\+\-\*\/]) (\d+)""".r
+    val result = value match {
+      case Some(Op(a, op, b)) => {
+        val fun: (Int, Int) => Int = op match {
+          case "+" => _ + _
+          case "-" => _ - _
+          case "*" => _ * _
+          case "/" => _ / _
+        }
+        Some(Seq(a, b).map(_.toInt).reduce(fun).toString)
+      }
+      case _ => None
+    }
+    result.map(res =>
+      Accept(Some(res), tokens)).getOrElse(
+        Reject("Expected expression, got: \"" + value, tokens))
   }
 }
--- src/main/scala/about/monads/scala/Step.scala
+++ src/main/scala/about/monads/scala/Step.scala
@@ -6,11 +6,12 @@ import java.util.StringTokenizer
  * Parse Step monad.
  */
 abstract sealed trait Step {
+  val value: Option[String]
   val tokens: List[String]
 
-  def >>=(f: List[String] => Step): Step = flatMap(f)
-  def flatMap(f: List[String] => Step): Step
-  def map(f: List[String] => List[String]): Step
+  def >>=(f: (Option[String], List[String]) => Step): Step = flatMap(f)
+  def flatMap(f: (Option[String], List[String]) => Step): Step
+  def map(f: (Option[String], List[String]) => List[String]): Step
 
   // see ReaderT monad transformer aka Kleisli
   def orElse(step: Step): Step
@@ -19,19 +20,25 @@ abstract sealed trait Step {
 object Step {
   def apply(tokens: String) = {
     val tk = new StringTokenizer(tokens, " +-*/()", true)
-    Accept(Iterator.continually(tk).takeWhile(
+    Accept(None, Iterator.continually(tk).takeWhile(
       _.hasMoreTokens).map(_.nextToken).filter(" " !=).toList)
   }
 }
 
-case class Accept(tokens: List[String]) extends Step {
-  def flatMap(f: List[String] => Step): Step = f(tokens)
-  def map(f: List[String] => List[String]): Step = Accept(f(tokens))
+case class Accept(value: Option[String], tokens: List[String]) extends Step {
+  def flatMap(f: (Option[String], List[String]) => Step): Step = f(value, tokens)
+  def map(f: (Option[String], List[String]) => List[String]): Step = Accept(f(value, tokens))
   def orElse(step: Step) = this
 }
 
+object Accept {
+  def apply(tokens: List[String]): Accept = Accept(None, tokens)
+}
+
 case class Reject(error: String, tokens: List[String]) extends Step {
-  def flatMap(f: List[String] => Step): Step = this
-  def map(f: List[String] => List[String]) = this
+  val value = None
+
+  def flatMap(f: (Option[String], List[String]) => Step): Step = this
+  def map(f: (Option[String], List[String]) => List[String]) = this
   def orElse(step: Step) = step
 }
